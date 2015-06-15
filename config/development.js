3772867d6bb82f88341c716912663facc118a46f
slavik <slavik990@gmail.com>
2015-06-15 11:08:55 +0300

    bg

--- config/development.js
+++ config/development.js
@@ -1,6 +1,6 @@
 'use strict';
 
-process.env.HOST = 'http://134.249.164.53:6278';
-process.env.PORT = 6278;
+process.env.HOST = 'http://134.249.164.53:8838';
+process.env.PORT = 8838;
 process.env.DB_HOST = 'localhost';
 process.env.DB_NAME = 'demoDB';
