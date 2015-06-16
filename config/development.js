01bbe40063c62ea935563a927d7fdc0448bdd183
slavik <slavik990@gmail.com>
2015-06-16 15:35:02 +0300

    bg

--- config/development.js
+++ config/development.js
@@ -2,5 +2,5 @@
 
 process.env.HOST = 'http://134.249.164.53:8838';
 process.env.PORT = 8838;
-process.env.DB_HOST = '192.168.88.250';
+process.env.DB_HOST = 'localhost';
 process.env.DB_NAME = 'demoDB';
