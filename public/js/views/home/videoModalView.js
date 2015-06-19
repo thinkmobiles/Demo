f7c03d04e48491996d1fe4aaf13b154608fa9f70
slavik <slavik990@gmail.com>
2015-06-19 11:46:18 +0300

    Merge branch 'develop'
    Conflicts:
    	config/development.js

--- public/js/views/home/videoModalView.js
+++ public/js/views/home/videoModalView.js
@@ -1,7 +1,8 @@
 define([
     'text!templates/home/videoModalTemplate.html',
+    'text!templates/home/relatedVideo.html',
     "models/userModel"
-], function ( modalTemplate, UserModel) {
+], function ( modalTemplate, relatedVideo, UserModel) {
 
     var View;
 
@@ -11,7 +12,8 @@ define([
             "click .ui-dialog-titlebar-close":"closeDialog",
             "click .continue":"register",
 			"click .questionSection table .checkbox" : "checkedQuestion",
-			"ended .mainVideo":"endedMainVideo"
+			"ended .mainVideo":"endedMainVideo",
+			"click .showSurvay":"showSurvay"
         },
 
 
@@ -24,6 +26,12 @@ define([
 			$(".videoSection").hide();
 			$(".questionSection").show();
             //sendAjax();
+		},
+
+		showSurvay:function(e){
+			$(".questionSection").hide();
+			$(".relatedVideo").show();
+			$(".relatedVideo").html(_.template(relatedVideo)(this.company.toJSON().survey[0]));
 		},
 
 		checkedQuestion: function(e){
