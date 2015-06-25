9c941b71e08987c2a01d150552df2bf28b532c08
Ivan Lyah <johnnye.be@gmail.com>
2015-06-25 13:36:54 +0300

    new app

--- handlers/users.js
+++ handlers/users.js
@@ -643,6 +643,8 @@ var routeHandler = function (db) {
                 if (err) {
                   return  callback(err);
                 }
+                //ToDo: pdf preview
+
                 //-----------------------------------------------------------------
               /*  pdfutils(file.path, function(err, doc) {
                     doc[0].asPNG({maxWidth: 200, maxHeight: 300}).toFile(url+sep+file.originalFilename.split(sep).pop().slice(0, -4));
