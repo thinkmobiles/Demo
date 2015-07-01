2e657ddef323e9cdd98778eae63f6fc33170cc93
john <johnnye.be@gmail.com>
2015-07-01 11:22:16 +0300

    error fix

--- constants/jumplead.js
+++ constants/jumplead.js
@@ -3,6 +3,6 @@ module.exports = {
     ACCESS_TOKEN_URL: 'https://account.mooloop.com/oauth/access_token',
     REFRESH_TOKEN_URL: 'https://account.mooloop.com/oauth/access_token',
     CONTACTS_URL: 'https://app.jumplead.com/api/v1/contacts',
-    CONTACTS_SCOPE: 'jumplead.contacts'
-
+    CONTACTS_SCOPE: 'jumplead.contacts',
+    CHECK_USER_URL: 'https://app.jumplead.com/api/v1/users/me'
 };
