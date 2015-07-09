9c0a1d36cb0adea4929e2dd73a74215446bb25b5
slavik990 <slavik990@gmail.com>
2015-07-09 14:19:05 +0300

    Merge branch 'develop'
    Conflicts:
    	public/js/views/registration/registrationView.js

--- public/js/views/modal/watchVideoView.js
+++ public/js/views/modal/watchVideoView.js
@@ -302,6 +302,7 @@ define([
 				self.dialog.remove();
 				Backbone.history.navigate("#/chooseImportant/"+self.videoId+"/"+self.userId, {trigger: true});
 			});
+			FB.XFBML.parse();
 			return this;
 		}
 
