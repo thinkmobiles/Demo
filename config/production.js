e196a9a23f5795f0088a6b06993a15ef51a0b1c7
john <johnnye.be@gmail.com>
2015-07-27 13:38:53 +0300

    uploadBar

--- config/production.js
+++ config/production.js
@@ -1,3 +1,5 @@
+'use strict';
+
 process.env.HOST = 'http://127.0.0.1:8838';
 process.env.WEB_HOST = 'http://134.249.164.53:8838';
 process.env.PORT = 8838;
@@ -9,6 +11,8 @@ process.env.CLIENT_SECRET = "cPtrcBklxNUEnVL3VBETsKhsDGQHGF73wkYcwWU9";
 process.env.CLIENT_ID = "uemnB2ZAA92gv5CoTCHc";
 process.env.REDIRECT_URI = "http://134.249.164.53:8838/redirect";
 process.env.HOME_PAGE = "http://134.249.164.53:8838/#/home/";
+
+process.env.mailerService = 'Gmail';
 process.env.SUPER_ADMIN_EMAIL = "johnnye.be@gmail.com";
 process.env.mailerUserName = "gogi.gogishvili";
 process.env.mailerPassword = "gogi123456789";
