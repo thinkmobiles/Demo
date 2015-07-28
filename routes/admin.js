b022ed2a5347edec2ad09c4c45f6af98766dfc2b
john <johnnye.be@gmail.com>
2015-07-28 09:52:58 +0300

    admin future

--- routes/admin.js
+++ routes/admin.js
@@ -9,8 +9,9 @@ module.exports = function (db) {
 
     var handler = new AnalyticHandler(db);
     var session = new SessionHandler(db);
-    router.get('/confirm', handler.confirm);
-    router.get('/users', session.isAuthenticatedAdmin, handler.users);
+    router.get('/confirm', handler.confirmUser);
+    router.get('/users/pending', session.isAuthenticatedAdmin, handler.pendingUsers);
+    router.get('/users/confirmed', session.isAuthenticatedAdmin, handler.confirmedUsers);
     router.post('/changePass', session.isAuthenticatedAdmin, handler.changePass);
     router.delete('/user', session.isAuthenticatedAdmin, handler.remove);
 
