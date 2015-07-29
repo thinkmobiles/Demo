ceb53e35cac17b140f8a05d163d6fa1c33b612eb
slavik990 <slavik990@gmail.com>
2015-07-29 14:46:04 +0300

    Merge branch 'develop'
    Conflicts:
    	public/js/views/modal/watchVideoView.js
    	public/templates/home/videoModalTemplate.html

--- public/js/views/forgot/forgotView.js
+++ public/js/views/forgot/forgotView.js
@@ -1,16 +1,51 @@
 define([
-    'text!templates/forgot/forgotTemplate.html'
-], function (Template) {
+    'text!templates/forgot/forgotTemplate.html',
+	"validation"
+], function (Template, validation) {
     var View = Backbone.View.extend({
 
 		el:"#wrapper",
         events:{
+			"click .buttons .cancel" :"back" ,
+			"click .buttons .registerBtn" :"send" ,
         },
         initialize: function () {
             this.render();
         },
 		
+		back: function(){
+			Backbone.history.navigate("#/home", {trigger: true});
+		},
 
+		
+		send: function(){
+			var self = this;
+			var isError = false;
+			
+			self.$el.find(".registration .error").removeClass("error");
+			
+			if (!validation.validEmail(self.$el.find(".registration .email").val())){
+				isError = true;
+				self.$el.find(".registration .email").addClass("error");
+			}
+			if (isError)return;
+			
+			$.ajax({
+                url: "/forgot",
+                type: "POST",
+                dataType: 'json',
+                data: {
+                    email: self.$el.find(".registration .email").val()
+                },
+                success: function (response) {
+					Backbone.history.navigate("#/home",{ trigger:true })
+					App.notification("Recover link sendad in your email");
+                },
+                error: function (err) {
+					App.notification("Some error");
+				}
+            });
+		},
 		
         render: function () {
             this.$el.html(_.template(Template));
