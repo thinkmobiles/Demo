49cb79f85b9a67c7bf894e5a032ef1e6286d8ecb
slavik990 <slavik990@gmail.com>
2015-08-03 11:50:24 +0300

    Merge branch 'develop'
    Conflicts:
    	public/js/views/modal/watchVideoView.js
    	public/templates/home/videoModalTemplate.html

--- public/js/models/contentModel.js
+++ public/js/models/contentModel.js
@@ -5,7 +5,11 @@ define(['validation'], function (validation) {
     var Model = Backbone.Model.extend({
         idAttribute: "_id",
         url: function () {
-            return "/content/" + this.get('_id')+"/"+this.get('userId');
+			if (this.get('_id') && this.get('userId')){
+				return "/content/" + this.get('_id')+"/"+this.get('userId');
+			}else{
+				return "/content";
+			}
         },
         initialize: function () {
             this.on('invalid', function (model, errors) {
