cbd1902cbe26e0b265742c874234af37eb59bdea
john <johnnye.be@gmail.com>
2015-08-14 08:27:43 +0300

    Merge branch 'develop'
    Conflicts:
    	public/js/views/registration/registrationView.js

--- public/js/views/modal/watchVideoView.js
+++ public/js/views/modal/watchVideoView.js
@@ -75,12 +75,12 @@ define([
 		showList: function (e) {
 			var self = this;
 			self.$el.find(".hoverList span").stop().animate({ opacity: 0 }, 100);
-			self.$el.find(".listVideo").stop().show("drop", { direction: "right" }, 500);
+			self.$el.find(".listVideo").stop().animate({ opacity: 1, marginRight: 0 }, 500);
 		},
 
 		hideList: function (e) {
 			var self = this;
-			self.$el.find(".listVideo").stop().delay(1500).hide("drop", { direction: "right" }, 500);
+			self.$el.find(".listVideo").stop().delay(1500).animate({ opacity: 0, marginRight: -130 }, 500);
 			self.$el.find(".hoverList span").stop().animate({ opacity: 0 }, 100);
 		},
 
@@ -345,7 +345,7 @@ define([
 				width: 1180
 			});
 			setTimeout(function () {
-				self.$el.find(".listVideo").stop().hide("drop", { direction: "right" }, 500);
+				self.$el.find(".listVideo").stop().animate({ opacity: 0, marginRight: -130 }, 500);
 			}, 3000);
 
 			this.$el.find("video").on('ended',function(){
