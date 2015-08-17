a26e407cdbce65c2a1d4007ebbaa61b5421bb6ec
john <johnnye.be@gmail.com>
2015-08-17 17:34:41 +0300

    Merge branch 'develop'
    Conflicts:
    	public/js/views/registration/registrationView.js

--- config/production.js
+++ config/production.js
@@ -1,14 +1,14 @@
-process.env.HOST = 'http://127.0.0.1:8838';
+process.env.HOST = 'http://127.0.0.1:8838'; //do not change
 process.env.WEB_HOST = 'http://134.249.164.53:8838';
 process.env.PORT = 8838;
 
 process.env.DB_HOST = 'localhost';
-//process.env.DB_HOST = '127.0.0.1';
 process.env.DB_NAME = 'rocketDB';
 process.env.CLIENT_SECRET = "cPtrcBklxNUEnVL3VBETsKhsDGQHGF73wkYcwWU9";
 process.env.CLIENT_ID = "uemnB2ZAA92gv5CoTCHc";
 process.env.REDIRECT_URI = "http://134.249.164.53:8838/redirect";
 process.env.HOME_PAGE = "http://134.249.164.53:8838/#/home/";
+
 process.env.mailerService = 'Gmail';
 process.env.mailerUserName = "gogi.gogishvili";
 process.env.mailerPassword = "gogi123456789";
