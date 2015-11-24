44c52f041019376915f906da4b06bd24d2aa5e73
john <johnnye.be@gmail.com>
2015-11-24 11:19:55 +0200

    update url when register user

--- config/production.js
+++ config/production.js
@@ -4,8 +4,8 @@ process.env.PORT = 8838;
 
 process.env.DB_HOST = 'localhost';
 process.env.DB_NAME = 'rocketDB';
-process.env.CLIENT_SECRET = "cPtrcBklxNUEnVL3VBETsKhsDGQHGF73wkYcwWU9";
-process.env.CLIENT_ID = "uemnB2ZAA92gv5CoTCHc";
+process.env.CLIENT_SECRET = "wXtdoiQz3tIpHo98R0Bhrc4E09IDmMccZtoT0Ieq";
+process.env.CLIENT_ID = "S2aUNWlBd2HRMKsQzZqW";
 process.env.REDIRECT_URI = "http://projects.thinkmobiles.com:8838/redirect";
 process.env.HOME_PAGE = "http://projects.thinkmobiles.com:8838/#/home/";
 
