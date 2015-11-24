b96708c73a0a22bda8d0c581e504daeb1168735c
john <johnnye.be@gmail.com>
2015-11-24 11:48:57 +0200

    update url when register user

--- config/development.js
+++ config/development.js
@@ -7,9 +7,9 @@ process.env.PORT = 8838;
 //process.env.DB_HOST = '192.168.88.250';//
 process.env.DB_HOST = '127.0.0.1'; //local
 process.env.DB_NAME = 'rocketDB';
-process.env.CLIENT_SECRET = "sCkxGw1dWL4j1QrOnNmT6ZOv9feBqFhUbe1uTg4Y";
-process.env.CLIENT_ID = "FcDOCBsnZ2TtKbHTGULY";
-process.env.REDIRECT_URI = "http://demo.com:8838/redirect";
+process.env.CLIENT_SECRET = "cPtrcBklxNUEnVL3VBETsKhsDGQHGF73wkYcwWU9";
+process.env.CLIENT_ID = "uemnB2ZAA92gv5CoTCHc";
+process.env.REDIRECT_URI = "http://127.0.0.1:8838/redirect";
 process.env.HOME_PAGE = "http://demo.com:8838/#/home/";
 
 process.env.mailerService = 'Gmail';
