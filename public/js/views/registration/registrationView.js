9510b378c356205341c40c8a879d671a76cba1cf
john <johnnye.be@gmail.com>
2015-11-26 18:00:19 +0200

    create user error message fix

--- public/js/views/registration/registrationView.js
+++ public/js/views/registration/registrationView.js
@@ -155,10 +155,10 @@ define([
                 success: function (model) {
                     console.log(model);
                     //ToDo: Develop
-                    //window.location = 'https://account.mooloop.com/oauth/authorize?response_type=code&client_id=FcDOCBsnZ2TtKbHTGULY&redirect_uri=http://demo.com:8838/redirect&scope=jumplead.contacts,jumplead.personal';
+                    window.location = 'https://account.mooloop.com/oauth/authorize?response_type=code&client_id=FcDOCBsnZ2TtKbHTGULY&redirect_uri=http://demo.com:8838/redirect&scope=jumplead.contacts,jumplead.personal';
                     //
                     //ToDo: Production
-                    window.location = 'https://account.mooloop.com/oauth/authorize?response_type=code&client_id=S2aUNWlBd2HRMKsQzZqW&redirect_uri=http://projects.thinkmobiles.com:8838/redirect&scope=jumplead.contacts,jumplead.personal';
+                    //window.location = 'https://account.mooloop.com/oauth/authorize?response_type=code&client_id=S2aUNWlBd2HRMKsQzZqW&redirect_uri=http://projects.thinkmobiles.com:8838/redirect&scope=jumplead.contacts,jumplead.personal';
                 },
                 error: function (err) {
                     console.log(err);
